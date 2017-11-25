8452224b1e6d999ada5f79367a1a4e659391c178
dcorto <daviscorto@gmail.com>
2017-11-25 18:37:12 +0100

    Test for the HelloWorldController done!

--- module/Application/test/Controller/HelloWorldControllerTest.php
+++ module/Application/test/Controller/HelloWorldControllerTest.php
@@ -8,16 +8,32 @@
 namespace ApplicationTest\Controller;
 
 use Application\Controller\HelloWorldController;
+use Zend\Stdlib\ArrayUtils;
 use Zend\Test\PHPUnit\Controller\AbstractConsoleControllerTestCase;
 
 class HelloWorldTestController extends AbstractConsoleControllerTestCase
 {
     public function setUp()
     {
-
+        // The module configuration should still be applicable for tests.
+        // You can override configuration here with test case specific values,
+        // such as sample view templates, path stacks, module_listener_options,
+        // etc.
+        $configOverrides = [];
+        $this->setApplicationConfig(ArrayUtils::merge(
+            include __DIR__ . '/../../../../config/application.config.php',
+            $configOverrides
+        ));
+        parent::setUp();
     }
 
     public function testIndexActionShowHelloWorld(){
-        $this->assertTrue(true, "TRUE");
+        $this->dispatch('hello-world');
+
+        $this->assertModuleName('application');
+        $this->assertControllerName(HelloWorldController::class);
+        $this->assertControllerClass('HelloWorldController');
+        $this->assertMatchedRouteName('hello-world');
+        $this->assertConsoleOutputContains('Hello World');
     }
 }
